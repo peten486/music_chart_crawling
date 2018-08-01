8533baf8448ff6272b28c6bb0f27c28ca85ebca2
peten <peten486@gmail.com>
2018-08-01 23:42:13 +0900

    멜론차트 크롤링2

--- melon_chart.py
+++ melon_chart.py
@@ -4,6 +4,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from bs4 import BeautifulSoup
+from datetime import datetime  
+
 import requests
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
@@ -13,6 +15,7 @@ soup = BeautifulSoup(melon.text, 'html.parser')
 titles = soup.select('#lst50 > td > div > div > div.ellipsis.rank01 > span > a')
 artist = soup.select('#lst50 > td > div > div > div.ellipsis.rank02 > span')
 
-print('실시간 멜론 차트\n')
+print('실시간 멜론 차트')
+print('현재시각 :  %d-%d-%s' % (datetime.now().year, datetime.now().month, datetime.now().day))
 for i in range(50):
-        print( '%3d위: %s - %s'%(i+1, titles[i].text, artist[i].text))
+        print( '%3d위: %s - %2d'%(i+1, titles[i].text, artist[i].text))
